9872c1ce2c2db13f28d1f1e565e6197754cfc3c3
Magnitovich <newbrain31@gmail.com>
2017-01-25 16:17:08 +0200

    allFabric start

---
+++ src/main/resources/static/js/fabric/curtain.js
@@ -0,0 +1,55 @@
+function buyYachtFromImg() {
+
+    var selectedForEdit = $('input:hidden')
+
+
+    window.location.href ="buyYachts?id="+selectedForEdit[0].id;
+
+}
+
+function addNewCurtain(){
+
+    window.location.href ="addInfoAboutNewCurtain";
+}
+
+function editInfoCurtain() {
+
+    var selectedForEdit = $('input:checked')
+
+    if (selectedForEdit.length>1) {
+        alert("You selected more one yacht")
+
+    }else if (selectedForEdit.length==0) {
+        alert("You don't selected anyone yacht")
+    } else {
+
+        window.location.href ="addInfoAboutNewCurtain?id="+selectedForEdit[0].id;
+    }
+}
+
+function deleteSelectedElementsCurtain() {
+
+    var selectedYachtForDelete = [];
+    var i = 0;
+
+    var arrayCheckboxClicked = $('input:checked')
+
+    arrayCheckboxClicked.each(function(){
+        var hasNextt = $(this);
+
+        selectedYachtForDelete[i] = hasNextt[0].id;
+        i++;
+    });
+    $.ajax({   //тип запроса
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        type:"POST", //это типа method
+        data: JSON.stringify(selectedYachtForDelete), //грубо говоря это Серриализация
+        url: '/deleteYacht/DELETE',
+        success: function(msg){  //msg - показывает ответ с сервера
+            window.location.href = "/curt"
+        }
+    })
+}
