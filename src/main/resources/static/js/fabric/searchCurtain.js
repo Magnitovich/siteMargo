960a198cb4eea67a1f0960b61fee35371f2d9a43
Magnitovich <newbrain31@gmail.com>
2017-02-14 19:24:16 +0200

    search Curtain 70%

--- src/main/resources/static/js/fabric/searchCurtain.js
+++ src/main/resources/static/js/fabric/searchCurtain.js
@@ -3,35 +3,35 @@ $(document).ready(function() {
     var arrayMax = [];
     var maxPrice;
     var minPrice;
-   $("div[id='filterPrice'] input").change(function(){
+   //$("div[id='filterPrice'] input").change(function(){
 
-
-       arrayMax = $("[class^=priceSSS]");
-       for (i=0; i<arrayMax.length; i++){
-           arrayPrice[i] = arrayMax[i].innerHTML;
-       }
-       console.log(arrayPrice)
-       maxPrice = Math.max.apply(null, arrayPrice);
-       minPrice =  Math.min.apply(null, arrayPrice);
-
-   });
+   //});
     $("div[class='searchColor'] input").change(function () {
-
+        arrayMax = $("[class^=priceSSS]");
+        for (i=0; i<arrayMax.length; i++){
+            arrayPrice[i] = arrayMax[i].innerHTML;
+        }
+        console.log(arrayPrice)
+        maxPrice = Math.max.apply(null, arrayPrice);
+        minPrice =  Math.min.apply(null, arrayPrice);
 
         if($("#filterColor input:checked").length == 0 && $("#searchPaint input:checked").length == 0 &&
-                 $("#filterPrice input:checked").length == 0 ){
+                 $("#filterPrice input:checked").length == 0  && $("#filterStructure input:checked").length == 0
+            && $("#filterHeight input:checked").length == 0){
 
             $('.color').show();
 
         }else if($("#filterColor input:checked").length == 0 && $("#searchPaint input:checked").length > 0 &&
-                         $("#filterPrice input:checked").length == 0 ){
+                $("#filterPrice input:checked").length == 0 && $("#filterStructure input:checked").length == 0
+            && $("#filterHeight input:checked").length == 0){
 
             $('.color').show();
             $("#searchPaint input:not(:checked)").each(function() {
                 $('.' + $(this).attr('value')).hide();
             });
         }else if($("#filterColor input:checked").length > 0 && $("#searchPaint input:checked").length == 0 &&
-                        $("#filterPrice input:checked").length == 0 ){
+            $("#filterPrice input:checked").length == 0 && $("#filterStructure input:checked").length == 0
+            && $("#filterHeight input:checked").length == 0){
 
             $('.color').show();
             selectedMoreThreeCheckboxColor();
@@ -40,35 +40,85 @@ $(document).ready(function() {
                 $('.' + $(this).attr('value')).hide();
             });
         }else if($("#filterColor input:checked").length == 0 && $("#searchPaint input:checked").length == 0 &&
-            $("#filterPrice input:checked").length > 0) {
+            $("#filterPrice input:checked").length == 0 && $("#filterStructure input:checked").length > 0
+            && $("#filterHeight input:checked").length == 0){
+            $('.color').show();
+
+            $("#filterStructure input:not(:checked)").each(function() {
+                var selectedStr = $(this).val();
+                $('.' + selectedStr).hide();
+            });}
+        else if($("#filterColor input:checked").length == 0 && $("#searchPaint input:checked").length == 0 &&
+            $("#filterPrice input:checked").length == 0 && $("#filterStructure input:checked").length == 0
+            && $("#filterHeight input:checked").length > 0){
+
+            $('.height').show();
+
+            $("#filterHeight input:not(:checked)").each(function() {
+                var selectedStr = $(this).val();
+                $('.' + selectedStr.text).hide();
+            });
+        }else if($("#filterColor input:checked").length == 0 && $("#searchPaint input:checked").length == 0
+                && $("#filterPrice input:checked").length > 0  && $("#filterStructure input:checked").length == 0
+            && $("#filterHeight input:checked").length == 0){
 
             $('.color').show();
-            //switch ()
-            //$("#filterPrice input:not(:checked)").each(function() {
-            //    //console.log(this,$(this).attr('value'),$('.'+$(this).attr('value')))
-            //    $('.' + $(this).attr('value')).hide();
-
-            console.log("max: "+ maxPrice);
-            console.log("min: "+ minPrice);
-            //});
-        price();
+            price();
 
         }else{
             selectedMoreThreeCheckboxColor();
-            $('.color').show();
+
+            if( $("#filterPrice input:checked").length > 0){
+                $('.color').show();
+            } else {$('.color').show();}
 
             $("#searchPaint input:not(:checked)").each(function() {
-                var notCheckedPaint = $(this).val();
-                $('.' + notCheckedPaint).hide();
+                if ($("#searchPaint input:checked").length == 0){
+
+                } else {
+                    checkOnIf();
+                    var notCheckedPaint = $(this).val();
+                    //$(this).attr('value')
+                    $('.' + notCheckedPaint).hide();
+                }
             });
 
             $("#filterColor input:not(:checked)").each(function() {
-                //console.log(this,$(this).attr('value'),$('.'+$(this).attr('value')))
-                $('.' + $(this).attr('value')).hide();
+                if ($("#filterColor input:checked").length == 0){
+
+                } else {
+                    checkOnIf();
+                    var notCheckedColor = $(this).val();
+                    //console.log(this,$(this).attr('value'),$('.'+$(this).attr('value')))
+
+                    $('.' + notCheckedColor).hide();
+                }
+            });
+            $("#filterHeight input:not(:checked)").each(function() {
+                if ($("#filterHeight input:checked").length == 0){
+
+                } else {
+                    checkOnIf();
+                    var notCheckedColor = $(this).val();
+                    //console.log(this,$(this).attr('value'),$('.'+$(this).attr('value')))
+
+                    $('.height' + notCheckedColor).hide();
+                }
             });
 
+            $("#filterStructure input:not(:checked)").each(function() {
+                if ($("#filterStructure input:checked").length == 0){
+
+                } else {
+                    checkOnIf();
+                    var notCheckedColor = $(this).val();
+
+                    $('.' + notCheckedColor).hide();
+                }
+            });
         }
     });
+    //IF MORE 3 CHECKBOX-CHECKED DOING DISABLE OTHER CHECKBOX
     function selectedMoreThreeCheckboxColor(){
             if($("#filterColor input[type=checkbox]:checked").length >= 3 ){
                 $('#filterColor input[type=checkbox]:not(:checked)').attr('disabled', "disabled");
@@ -78,62 +128,59 @@ $(document).ready(function() {
     }
 
     function price() {
-        var that = this;
-        //var firstIntervalStart = 0;
-        //var firstIntervalEnd = 0;
-        //var secondIntervalStart = 0;
-        //var secondIntervalEnd = 0;
-
-        hideAllSpans();
+        var that = $(this);
+        var firstIntervalStart = 0;
+        var firstIntervalEnd = 0;
+        var secondIntervalStart = 0;
+        var secondIntervalEnd = 0;
 
         var oneChecked = $("#price01").is(':checked');
         var twoChecked = $("#price02").is(':checked');
         var threeChecked = $("#price03").is(':checked');
 
-        //if (oneChecked) {
-        //    firstIntervalStart = minPrice;
-        //    firstIntervalEnd = 800;
-        //    if (twoChecked) {
-        //        firstIntervalEnd = 1600;
-        //        if (threeChecked) {
-        //            firstIntervalEnd = maxPrice;
-        //        }
-        //    }
-        //    else if (threeChecked) {
-        //        secondIntervalStart = 1600;
-        //        secondIntervalEnd = maxPrice;
-        //    }
-        //}
-        //else if (twoChecked) {
-        //    firstIntervalStart = 800;
-        //    firstIntervalEnd = 1600;
-        //    if (threeChecked) {
-        //        firstIntervalEnd = maxPrice;
-        //    }
-        //}
-        //else if (threeChecked) {
-        //    firstIntervalStart = 1600;
-        //    firstIntervalEnd = maxPrice;
-        //}
-        //
-        //for (var i = firstIntervalStart; i < firstIntervalEnd; i++) {
-        //    $($(".priceSSS")[i]).show();
-        //}
-        if (oneChecked){
-            for (i=0; i<arrayMax.length; i++){
-
+        if (oneChecked) {
+            firstIntervalStart = 0;
+            firstIntervalEnd = 800;
+            if (twoChecked) {
+                firstIntervalEnd = 1600;
+                if (threeChecked) {
+                    firstIntervalEnd = maxPrice;
+                }
+            }
+            else if (threeChecked) {
+                secondIntervalStart = 1600;
+                secondIntervalEnd = maxPrice;
             }
         }
+        else if (twoChecked) {
+            firstIntervalStart = 800;
+            firstIntervalEnd = 1600;
+            if (threeChecked) {
+                firstIntervalEnd = maxPrice;
+            }
+        }
+        else if (threeChecked) {
+            firstIntervalStart = 1600;
+            firstIntervalEnd = maxPrice;
+        }
 
+            for (i=0; i<arrayMax.length; i++) {
+                if (arrayPrice[i] > firstIntervalStart && arrayPrice[i] <= firstIntervalEnd) {
+                    $($(".color")[i]).show();
+                } else { $($(".color")[i]).hide();}
+            }
         if (secondIntervalStart != 0) {
-            for (var i = secondIntervalStart; i < secondIntervalEnd; i++) {
-                $($(".priceSSS")[i]).show();
+            for (i = 0; i < arrayMax.length; i++) {
+                if (arrayPrice[i] > secondIntervalStart && arrayPrice[i] <= secondIntervalEnd) {
+                    console.log("secondIntervalStart < arrayPrice[i] <= secondIntervalEnd " + $($(".color")[i]));
+                    $($(".color")[i]).show();
+                } else { $($(".color")[i]).hide();}
             }
         }
-
-
-        function hideAllSpans() {
-            $(".priceSSS").hide();
+    }
+    function checkOnIf(){
+        if( $("#filterPrice input:checked").length > 0){
+            price();
         }
     }
 });
---
+++ src/main/resources/static/js/fabric/searchCurtainN.js
@@ -0,0 +1,232 @@
+
+$(document).ready(function() {
+
+    function showSelectedElement(arr){
+        console.log("start function showSelectedElement"+arr);
+        $('.color').hide();
+        arr.forEach(function(e){
+            console.log(" arr.forEach(function(e) ."+e.toString());
+            $("."+e.toString()).show();
+        });
+    }
+    var arrayPrice = [];
+    var arrayMax = [];
+    var maxPrice;
+    var minPrice;
+    var color = [];
+    var paint = [];
+    var structure = [];
+    var price = [];
+
+    $("div[class='searchColor'] input").change(function () {
+
+        arrayMax = $("[class^=priceSSS]");
+        for (i=0; i<arrayMax.length; i++){
+            arrayPrice[i] =arrayMax[i].innerHTML;
+        }
+        //console.log("Price: "+arrayPrice);
+        maxPrice = Math.max.apply(null, arrayPrice);
+        minPrice =  Math.min.apply(null, arrayPrice);
+
+
+        if($("#filterColor input:checked").length == 0 && $("#filterPrice input:checked").length == 0 &&
+            $("#searchPaint input:checked").length == 0
+            && $("#filterStructure input:checked").length == 0
+        ){
+            $('.color').show();
+        }else{
+            var color = ["Белый","Бежевый","Кремовый","Золотой", "Желтый","Оранжевый","Персиковый","Терракотовый",
+                "Кофейный", "Коричневый","Розовый","Красный", "Бордовый","Салатовый","Зеленый",
+                "Голубой", "Синий", "Фиолетовый", "Серый", "Черный"];
+            var paint = ["Абстрактный","Геометрический","Классический", "Однотонный","Полоска","Растительный"];
+            var structure = ["Лен","Нити","Двухслойная", "Жатая","Жаккард","Сатин", "Тафта", "Двухсторонняя","Бархат",
+                "Покрывальная"];
+            var price = [];
+
+            if($("#filterColor input:not(:checked)").length > 0){
+                $("#filterColor input:not(:checked)").each(function(){
+                    //удаляем 1 элемент, splice служит для добавления/удаления элем из массива
+                    //in this code we remove 1 elements. Splice(obj,1)
+                    color.splice(color.indexOf($(this).attr('value')),1);
+                })
+            }
+            console.log("color: "+color);
+
+            if($("#searchPaint input:not(:checked)").length > 0){
+                $("#searchPaint input:not(:checked)").each(function(){
+                    paint.splice(paint.indexOf($(this).attr('value')),1);
+                })
+            }
+            console.log("paint: "+paint);
+
+            if($("#filterPrice input:checked").length > 0){
+                var arr = [];
+                $("#filterPrice input:checked").each(function(){
+                    var s = $(this).attr('value');
+                    for (i=0; i<arrayMax.length; i++) {
+                    if(s == "1"){
+                        if (arrayPrice[i] > 0 && arrayPrice[i] <= 800) {
+                         arr = arr.concat((arrayPrice[i]));
+
+                            //console.log("Price ARRAY~ "+arr);
+                            //arr = arr.concat([10,20,30]);
+                        }
+                    }else if(s == "2") {
+                        if (arrayPrice[i] > 800 && arrayPrice[i] <= 1600) {
+                            arr = arr.concat(arrayPrice[i]);
+                            //arr = arr.concat([20,30,40]);
+                        }
+                        } else {
+                            if (arrayPrice[i] > 1600) {
+                                arr = arr.concat(arrayPrice[i]);
+
+                                //arr = arr.concat([30,40]);
+                            }
+                        }
+                    }
+                    });
+                console.log("Before Arr.forEach: "+arr);
+                //var result = $.map(arr, function (el) {
+                //    return el.replace(/\./,'\\\\.')
+                //});
+                //console.log(result);
+                arr.forEach(function(ele){
+                    if(price.indexOf(ele) == -1)
+                        price.push(ele);
+                });
+            }
+            console.log("number: "+price);
+
+            if(color.length == 0 && paint.length == 0 && price.length > 0){
+                $('.color').hide();
+                price.forEach(function(e){
+
+                    //console.log(e.toString().split(".").join("-"),$("."+e.toString().split(".").join("-")))
+                    $("."+e.replace( /(:|\.|\[|\]|,|=|@)/g, "\\$1" )).show();
+                });
+                //showSelectedElement(price)
+            }else if(color.length == 0 && paint.length > 0 && price.length == 0){
+
+                showSelectedElement(paint)
+            }else if(color.length > 0 && paint.length == 0 && price.length == 0){
+
+                showSelectedElement(color)
+            }else if(color.length > 0 && paint.length > 0 && price.length == 0){
+                var temp = [];
+                color.forEach(function(oe){
+                    paint.forEach(function(ie){
+                        temp.push(oe +"."+ie);
+                    });
+                });
+                showSelectedElement(temp);
+            }else if(color.length == 0 && paint.length > 0 && price.length > 0){
+                var temp = [];
+                paint.forEach(function(oe){
+                    price.forEach(function(ie){
+                        temp.push(oe +"."+ie);
+                    });
+                });
+                showSelectedElement(temp);
+            }else if(color.length > 0 && paint.length == 0 && price.length > 0){
+                var temp = [];
+                color.forEach(function(oe){
+                    price.forEach(function(ie){
+                        temp.push(oe +"."+ie);
+                    });
+                });
+                showSelectedElement(temp);
+            }else{
+                var temp = [];
+                color.forEach(function(oe){
+                    price.forEach(function(ie){
+                        paint.forEach(function(iie){
+                            temp.push(oe +"."+ie + "." + iie);
+                        });
+                    });
+                });
+                showSelectedElement(temp);
+            }
+        }
+    });
+});
+
+
+//$(document).ready(function() {
+//    var arrayNumber = [];
+//    var arrayMax = [];
+//
+//    $("div[class='searchSelectedNumber'] input").change(function () {
+//        arrayMax = $("[class^=number]");
+//        for (i=0; i<arrayMax.length; i++){
+//            arrayNumber[i] = arrayMax[i].innerHTML;
+//        }
+//        var firstIntervalStart = 0;
+//        var firstIntervalEnd = 0;
+//        var secondIntervalStart = 0;
+//        var secondIntervalEnd = 0;
+//        if (  $("#searchNumber input:checked").length == 0){
+//            $('.color').show();
+//        } else {
+//            var oneChecked = $("#number01").is(':checked');
+//            var twoChecked = $("#number02").is(':checked');
+//
+//            if (oneChecked) {
+//                firstIntervalStart = 0;
+//                firstIntervalEnd = 30;
+//                if (twoChecked) {
+//                    firstIntervalEnd = 40;
+//                }
+//            }
+//            else if (twoChecked) {
+//                firstIntervalStart = 20;
+//                firstIntervalEnd = 40;
+//            }
+//            for (i = 0; i < arrayMax.length; i++) {
+//                if (arrayNumber[i] > firstIntervalStart && arrayNumber[i] <= firstIntervalEnd) {
+//                    $($(".color")[i]).show();
+//                } else {
+//                    $($(".color")[i]).hide();
+//                }
+//            }
+//            if (secondIntervalStart != 0) {
+//                for (i = 0; i < arrayMax.length; i++) {
+//                    if (arrayNumber[i] > secondIntervalStart && arrayNumber[i] <= secondIntervalEnd) {
+//                        console.log("secondIntervalStart < arrayPrice[i] <= secondIntervalEnd " + $($(".color")[i]));
+//                        $($(".color")[i]).show();
+//                    } else {
+//                        $($(".color")[i]).hide();
+//                    }
+//                }
+//            }
+//        }
+//    });
+//    $("div[class='searchColor'] input").change(function () {
+//
+//        if($("#filterColor input:checked").length == 0 && $("#searchShape input:checked").length == 0
+//          ){
+//            $('.color').show();
+//        }else if($("#filterColor input:checked").length == 0 && $("#searchShape input:checked").length > 0){
+//            $('.color').show();
+//            $("#searchShape input:not(:checked)").each(function() {
+//                var k = $(this).val();
+//                $('.' + k).hide();
+//            });
+//        }else if($("#filterColor input:checked").length > 0 && $("#searchShape input:checked").length == 0){
+//            $('.color').show();
+//            $("#filterColor input:not(:checked)").each(function() {
+//
+//                $('.' + $(this).attr('value')).hide();
+//            });
+//        }else{
+//            $('.color').show();
+//
+//            $("#searchShape input:not(:checked)").each(function() {
+//                $('.' + $(this).attr('value')).hide();
+//            });
+//
+//            $("#filterColor input:not(:checked)").each(function() {
+//                $('.' + $(this).attr('value')).hide();
+//            });
+//        }
+//    });
+//});
